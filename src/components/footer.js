9077243849255fc334775d29ca89d84824344dea
Milind <milind.kulgod@gmail.com>
2025-07-28 15:14:48 -0500

    Footer year made dynamic

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
+  const years = new Date().getFullYear();
   return (
-    <footer style={{ backgroundColor: '#1c1c1c', padding: '1.5rem', textAlign: 'center' }}>
-      <p style={{ color: '#b3b3b3', margin: 0 }}>&copy; 2024 Milind Kulgod. Powered by React. All rights reserved.</p>
+    <footer
+      style={{
+        backgroundColor: "#1c1c1c",
+        padding: "1.5rem",
+        textAlign: "center",
+      }}
+    >
+      <p style={{ color: "#b3b3b3", margin: 0 }}>
+        &copy; {years} Milind Kulgod. Powered by React. All rights reserved.
+      </p>
     </footer>
   );
 };
